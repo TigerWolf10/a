ff98ac3b5327d807d6445e46a4f707a5050a24b6
TigerWolf10 <114867054+TigerWolf10@users.noreply.github.com>
2024-08-07 10:26:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ lwx = 0;
 lwy = 0;
 rwx = 0;
 rwy = 0;
+slw = 0;
+srw = 0;
+
+song = "";
+
+function preload(){
+    song = loadSound("music2.mp3");
+    song2 = loadSound("music.mp3");
+}
 
 function setup() {
     canvas = createCanvas(600,500);
@@ -20,6 +29,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+
+        slw = results[0].pose.keypoints[9].score;
+        console.log("Left Wrist Score = "+slw);
         lwx = results[0].pose.leftWrist.x;
         lwy = results[0].pose.leftWrist.y;
         console.log("Left wrist X = "+lwx+" Left wrist Y = "+lwy);
@@ -32,4 +44,22 @@ function gotPoses(results) {
 
 function draw() {
     image(video,0,0,600,500);
+
+    fill('#FF0000');
+    stroke('#FF0000');
+
+    if (slw > 0.2) {
+        circle(lwx,lwy,20);
+        song2.stop();
+        if (song.isPlaying()== false){
+            song.play();
+            document.getElementById("song_name").innerHTML = "Song name: Peter Pan";
+        }
+    }
+}  
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
